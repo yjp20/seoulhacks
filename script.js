4b83f645405316b96697e4970f342585a0c9e1f5
Young Jin Park <youngjinpark20@gmail.com>
2020-01-08 09:08:24 +0900

    More edits

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 window.addEventListener("load", () => {
- document.querySelector("body").classList.add("loaded");
-});
+	const cards = document.getElementsByClassName("card-expand")
+	const title = document.getElementById("main-title")
+
+
+	for (var i=0; i<cards.length; i++) {
+		cards[i].addEventListener("click", function () {
+			this.dataset.expanded = this.dataset.expanded == "true" ? false : true
+		}.bind(cards[i]))
+	}
+
+
+	charming(title)
+
+	anime({
+		targets: 'path',
+		strokeDashoffset: [anime.setDashoffset, 0],
+		easing: 'easeInOutSine',
+		duration: 1500,
+		delay: function(el, i) { return i * 250 },
+	});
+})
