3bbb4d3ddc0803e8077c479301f8533301687bc0
Young Jin Park <youngjinpark20@gmail.com>
2020-01-15 09:29:09 +0900

    Better graphic

--- script.js
+++ script.js
@@ -1,4 +1,69 @@
+/* from anime.js */
+function getAttribute(el, prop) {
+  return el.getAttribute(prop);
+}
+
+function getDistance(p1, p2) {
+  return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+}
+
+function getCircleLength(el) {
+  return Math.PI * 2 * getAttribute(el, 'r');
+}
+
+function getRectLength(el) {
+  return (getAttribute(el, 'width') * 2) + (getAttribute(el, 'height') * 2);
+}
+
+function getLineLength(el) {
+  return getDistance(
+    {x: getAttribute(el, 'x1'), y: getAttribute(el, 'y1')}, 
+    {x: getAttribute(el, 'x2'), y: getAttribute(el, 'y2')}
+  );
+}
+
+function getPolylineLength(el) {
+  const points = el.points;
+  let totalLength = 0;
+  let previousPos;
+  for (let i = 0 ; i < points.numberOfItems; i++) {
+    const currentPos = points.getItem(i);
+    if (i > 0) totalLength += getDistance(previousPos, currentPos);
+    previousPos = currentPos;
+  }
+  return totalLength;
+}
+
+function getPolygonLength(el) {
+  const points = el.points;
+  return getPolylineLength(el) + getDistance(points.getItem(points.numberOfItems - 1), points.getItem(0));
+}
+
+// Path animation
+
+function getTotalLength(el) {
+  if (el.getTotalLength) return el.getTotalLength();
+  switch(el.tagName.toLowerCase()) {
+    case 'circle': return getCircleLength(el);
+    case 'rect': return getRectLength(el);
+    case 'line': return getLineLength(el);
+    case 'polyline': return getPolylineLength(el);
+    case 'polygon': return getPolygonLength(el);
+  }
+}
+
+/* end */
+
+function reverseOffset(el) {
+	const pathLength = getTotalLength(el)
+  el.setAttribute('stroke-dasharray', pathLength)
+	return -pathLength
+}
+
 window.addEventListener("load", () => {
+
+	/* card expander */
+
 	const cards = document.getElementsByClassName("card-expand")
 	const title = document.getElementById("main-title")
 
@@ -9,35 +74,55 @@ window.addEventListener("load", () => {
 	}
 
 
-	charming(title)
+	/* svg animation */
 
 	var paths = [...document.querySelectorAll('path')]
 	var lines = document.querySelectorAll('line')
 	var polys = document.querySelectorAll('polyline')
+	var circles = [...document.querySelectorAll('circle')].concat(paths.filter(e => e.getBoundingClientRect().width <= 20 && e.getBoundingClientRect().height <= 20))
+	var not_circles = paths.filter(e => e.getBoundingClientRect().width > 20 || e.getBoundingClientRect().height > 20)
 
-	var other_paths = paths.filter(e => e.getBoundingClientRect().width > 10 || e.getBoundingClientRect().height > 10)
-	var outside_paths = [...other_paths.filter(e => !e.classList.contains("cls-20"))].map(e => e.parentElement)
-	var circle_paths = [...paths.filter(e => e.getBoundingClientRect().width <= 10 && e.getBoundingClientRect().height <= 10), ...document.querySelectorAll("circle")]
+	var combo = [...not_circles, ...lines, ...polys]
+	var combo_a = []
+	var combo_b = []
+	var ring1 = document.getElementById('Layer_1')
+	var ring1_paths = [...ring1.querySelectorAll('path')]
 
-	var all = [...other_paths, ...lines, ...polys]
+	combo.forEach(e => (ring1_paths.includes(e) || Math.random() < 0.7 ? combo_a : combo_b).push(e))
+	console.log(ring1_paths)
 
 	anime({
-		targets: all,
+		targets: combo_a,
 		strokeDashoffset: [anime.setDashoffset, 0],
 		easing: 'easeInOutSine',
 		duration: 1500,
-		delay: function(el, i) { return i * 15 },
+		direction: 'normal',
+		delay: function(el, i) { return i * 5 },
 	});
 	anime({
-		targets: outside_paths,
+		targets: combo_b,
+		strokeDashoffset: [reverseOffset, 0],
+		easing: 'easeInOutSine',
+		duration: 1500,
+		direction: 'normal',
+		delay: function(el, i) { return i * 5 },
+	});
+	anime({
+		targets: ring1,
 		rotate: 360,
 		easing: 'linear',
 		duration: 15000,
 		loop: true,
 	});
 	anime({
-		targets: [...all, ...circle_paths],
-		strokeDashoffset: [anime.setDashoffset, 0],
+		targets: combo,
+		opacity: 1,
+		easing: 'easeInOutSine',
+		duration: 700,
+		delay: function(el, i) { return i * 5 },
+	});
+	anime({
+		targets: circles,
 		opacity: 1,
 		easing: 'easeInOutSine',
 		duration: 1500,
